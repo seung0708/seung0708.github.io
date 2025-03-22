5030a44b8a00ff99ef9b004de85ace94f76aa3e4
Seung Hyun Kim <seung.kim0708@gmail.com>
2025-03-22 15:30:17 -0700

    cleaned up the context and styling a bit

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -8,12 +8,6 @@ const About = () => {
                 <h2>About Me</h2>
                 <p>I am Full Stack Developer living in Los Angeles, CA</p>
             </div>
-            <div className="about_intro">
-                <p>In 2019, I began my journey into tech from inside sales, graduating from a bootcamp in 2020. </p>
-                <p>In 2021, I got my first job as an Associate Software Developer, focusing primarily on database management.</p>
-                <p>In 2023, I left my role to support the family business, where I realized how web development could directly benefit businesses like my parents'</p>
-                <p>My background in sales, web development, and working in my parents' business has given me a unique perspective on creating practical, impactful solutions.</p>
-            </div>
         </div>
         </section>
   )
